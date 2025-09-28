ec5836feff08230c9f5b9f50c74776cbbcde8cfb
chauhan-varun <varunchauhan097@gmail.com>
2025-09-28 13:22:51 +0530

    Refactor Box1 and Box2 contracts to implement UUPS upgradeability and Ownable functionality. Update remapping in foundry.toml for OpenZeppelin contracts.

--- src/Box1.sol
+++ src/Box1.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract Box1 {
+contract Box1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 private value;
+
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
 
     function getValue() public view returns (uint256) {
         return value;
@@ -11,4 +24,8 @@ contract Box1 {
     function getVersion() public pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
--- src/Box2.sol
+++ src/Box2.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Box2 {
+contract Box2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 private value;
+
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
 
     function getValue() public view returns (uint256) {
         return value;
@@ -15,4 +28,8 @@ contract Box2 {
     function setValue(uint256 newValue) public {
         value = newValue;
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
